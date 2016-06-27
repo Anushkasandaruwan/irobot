2c81f0878e476f6370ccd95779d6c7d4aaa831e3
Anushka Samaranayake <anushkasandaruwan1@gmail.com>
2016-06-27 16:57:51 +0530

    code

---
+++ feature/feature/src/main/resources/dbscripts/mssql.sql
@@ -0,0 +1,9 @@
+
+-- -----------------------------------------------------
+-- Table `irobot_DEVICE`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS irobot_DEVICE (
+  irobot_DEVICE_ID VARCHAR(45) NOT NULL ,
+  DEVICE_NAME VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (irobot_DEVICE_ID) );
+
